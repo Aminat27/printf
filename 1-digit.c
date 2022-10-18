77234887170e754ea6b35d30332d8c26f4f9be4b
Aminat27 <adebayoaminat27@gmail.com>
2022-10-18 10:16:49 +0000

    fixed some changes

--- 1-digit.c
+++ 1-digit.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/**init_params - clears struct fields and reset buf
+/**
+ * init_params - clears struct fields and reset buf
  * @params: the parameters struct
  * @ap: the argument pointer
  * Return: void
