9f4c44e895862bdde8a248965b15900f0064fa20
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-10-21 18:27:44 -0700

    Updated

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _PRINTF_H
+#define _PRINTF_H
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -7,11 +7,17 @@
 #include <limits.h>
 #include <stdlib.h>
 
+
 #define OUTPUT_BUF_SIZE 1024
+
 #define BUF_FLUSH -1
+
 #define FIELD_BUF_SIZE 50
+
 #define NULL_STRING "(null)"
+
 #define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
 
@@ -30,21 +36,18 @@
  */
 typedef struct parameters
 {
-	unsigned int unsign			: 1;
-
-	unsigned int plus_flag		: 1;
-	unsigned int space_flag		: 1;
-	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag		: 1;
-	unsigned int minus_flag		: 1;
-
-	unsigned int width;
-	unsigned int precision;
-
-	unsigned int h_modifier		: 1;
-	unsigned int l_modifier		: 1;
-
+		unsigned int unsign			: 1;
+			unsigned int plus_flag		: 1;
+				unsigned int space_flag		: 1;
+					unsigned int hashtag_flag	: 1;
+						unsigned int zero_flag		: 1;
+							unsigned int minus_flag		: 1;
+								unsigned int width;
+									unsigned int precision;
+										unsigned int h_modifier		: 1;
+											unsigned int l_modifier		: 1;
 } params_t;
+
 /**
  * struct specifier - Struct token
  * @specifier: format token
@@ -52,12 +55,11 @@ typedef struct parameters
  */
 typedef struct specifier
 {
-	char *specifier;
-	int (*f)(va_list, params_t *);
-
+		char *specifier;
+			int (*f)(va_list, params_t *);
 } specifier_t;
 
-/* _put.c module */
+/* _putchar.c module */
 int _puts(char *str);
 int _putchar(int c);
 
@@ -68,7 +70,7 @@ int print_string(va_list ap, params_t *params);
 int print_percent(va_list ap, params_t *params);
 int print_S(va_list ap, params_t *params);
 
-/* converter.c module */
+/* number.c module */
 char *convert(long int num, int base, int flags, params_t *params);
 int print_unsigned(va_list ap, params_t *params);
 int print_address(va_list ap, params_t *params);
@@ -86,7 +88,7 @@ int print_HEX(va_list ap, params_t *params);
 int print_binary(va_list ap, params_t *params);
 int print_octal(va_list ap, params_t *params);
 
-/* char_printer.c module */
+/* converter.c module */
 int print_from_to(char *start, char *stop, char *except);
 int print_rev(va_list ap, params_t *params);
 int print_rot13(va_list ap, params_t *params);
@@ -97,10 +99,14 @@ int _strlen(char *s);
 int print_number(char *str, params_t *params);
 int print_number_right_shift(char *str, params_t *params);
 int print_number_left_shift(char *str, params_t *params);
-/* freebuff.c module */
+
+/* params.c module */
 void init_params(params_t *params, va_list ap);
-/* precision.c modoule */
+
+/* string_fields.c modoule */
 char *get_precision(char *p, params_t *params, va_list ap);
+
+
 /* _prinf.c module */
 int _printf(const char *format, ...);
 
